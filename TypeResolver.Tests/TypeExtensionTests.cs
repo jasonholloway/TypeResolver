0b0d226e152daa7057e8ced910963299f8c4b4a8
Jason Holloway <miceeatnicerice@hotmail.com>
2016-04-08 19:02:53 +0100

    Type.IsOpenGeneric() done

---
+++ TypeResolver.Tests/TypeExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TypeResolver.Tests
+{
+    [TestClass]
+    public class TypeExtensionTests
+    {
+
+        [TestMethod]
+        public void IsOpenGenericWorks() 
+        {
+            Assert.IsTrue(typeof(List<>).IsOpenGeneric(), "Fails on List<T> -> true");
+            Assert.IsFalse(typeof(int).IsOpenGeneric(), "Fails on int -> false");
+            Assert.IsFalse(typeof(List<int>).IsOpenGeneric(), "Fails on List<int> -> false");
+            Assert.IsTrue(typeof(List<>).MakeGenericType(typeof(List<>)).IsOpenGeneric(), "Fails on List<List<T>> -> true");
+        }
+        
+
+    }
+}
--- TypeResolver/TypeExtensions.cs
+++ TypeResolver/TypeExtensions.cs
@@ -8,10 +8,19 @@ namespace TypeResolver
 {
     public static class TypeExtensions
     {
+        
+        public static bool IsOpenGeneric(this Type @this) 
+        {
+            if(@this.IsGenericTypeDefinition) {
+                return true;
+            }
 
-        //...
-
-
+            if(!@this.IsGenericType) {
+                return false;
+            }
+            
+            return @this.GetGenericArguments().Any(a => a.IsOpenGeneric());
+        }
 
     }
 }
